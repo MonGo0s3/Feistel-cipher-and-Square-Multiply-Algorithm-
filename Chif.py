31d6b9b48451b48ba62054cea12a99309003e1be
MonGo0s3 <150374415+MonGo0s3@users.noreply.github.com>
2023-12-25 21:04:39 +0100

    Update Chif.py

--- Chif.py
+++ Chif.py
@@ -1,44 +1,32 @@
 #MMMDCXVIII
 
-def feistel_cipher(block, permutation, p_permutation, k1, k2):
-    # Appliquer la permutation initiale
-    permuted_block = permute(block, permutation)
+def feistel_cipher(N, permutation, left_shift_amount, right_shift_amount, k1, k2):
+    # Appliquer la permutation π
+    permuted_N = permute(N, permutation)
     
-    # Diviser le bloc en deux sous-blocs de 4 bits
-    g0 = permuted_block[:4]
-    d0 = permuted_block[4:]
+    # Diviser N en deux blocs de 4 bits
+    G0 = permuted_N[:4]
+    D0 = permuted_N[4:]
     
-    # Premier round
-    d1 = xor(p_permute(g0, p_permutation), k1)
-    g1 = xor(d0, or_operation(g0, k1))
+    # Premier Round
+    D1 = xor(permute(G0, [2, 0, 1, 3]), k1)
+    G1 = xor(D0, or_operation(G0, k1))
     
-    # Deuxième round
-    d2 = xor(p_permute(g1, p_permutation), k2)
-    g2 = xor(d1, or_operation(g1, k2))
+    # Deuxième Round
+    D2 = xor(permute(G1, [2, 0, 1, 3]), k2)
+    G2 = xor(D1, or_operation(G1, k2))
     
-    # Concaténer les sous-blocs
-    ciphertext = g2 + d2
+    # Concaténation des résultats
+    C = G2 + D2
     
-    # Appliquer l'inverse de la permutation initiale
-    ciphertext = permute(ciphertext, inverse_permutation(permutation))
+    # Appliquer l'inverse de la permutation π^(-1)
+    encrypted_text = permute(C, inverse_permutation(permutation))
     
-    return ciphertext
+    return encrypted_text
 
 def permute(block, permutation):
     # Appliquer la permutation à un bloc
     permuted_block = [block[i] for i in permutation]
-    
-    return permuted_block
-
-def inverse_permutation(permutation):
-    # Calculer l'inverse d'une permutation
-    inverse = [permutation.index(i) for i in range(len(permutation))]
-    
-    return inverse
-
-def p_permute(block, p_permutation):
-    # Appliquer la permutation P à un bloc
-    permuted_block = [block[i] for i in p_permutation]
     
     return permuted_block
 
@@ -54,23 +42,33 @@ def or_operation(block1, block2):
     
     return result
 
+def inverse_permutation(permutation):
+    # Calculer l'inverse d'une permutation
+    inverse = [0] * len(permutation)
+    for i, j in enumerate(permutation):
+        inverse[j] = i
+    
+    return inverse
+
 # Interaction avec l'utilisateur pour obtenir les données d'entrée
-block = input("Entrez le bloc binaire de longueur 8 : ")
-block = [int(bit) for bit in block]  # Convertir le bloc en une liste d'entiers
+N = input("Entrez le bloc N binaire de longueur 8 : ")
+N = [int(bit) for bit in N]  # Convertir le bloc N en une liste d'entiers
 
 permutation = input("Entrez la permutation de longueur 8 (indices séparés par des espaces) : ")
 permutation = [int(index) for index in permutation.split()]  # Convertir la permutation en une liste d'entiers
 
-p_permutation = [2, 0, 1, 3]  # Permutation P
+left_shift_amount = int(input("Entrez l'ordre du décalage à gauche : "))
+right_shift_amount = int(input("Entrez l'ordre du décalage à droite : "))
 
-k1 = input("Entrez la sous-clé k1 de longueur 4 : ")
-k1 = [int(bit) for bit in k1]  # Convertir la sous-clé en une liste d'entiers
+k1 = input("Entrez la sous-clé k1 binaire de longueur 4 : ")
+k1 = [int(bit) for bit in k1]  # Convertir la sous-clé k1 en une liste d'entiers
 
-k2 = input("Entrez la sous-clé k2 de longueur 4 : ")
-k2 = [int(bit) for bit in k2]  # Convertir la sous-clé en une liste d'entiers
+k2 = input("Entrez la sous-clé k2 binaire de longueur 4 : ")
+k2 = [int(bit) for bit in k2]  # Convertir la sous-clé k2 en une liste d'entiers
 
 # Chiffrement de Feistel
-ciphertext = feistel_cipher(block, permutation, p_permutation, k1, k2)
+encrypted_text = feistel_cipher(N, permutation, left_shift_amount, right_shift_amount, k1, k2)
 
 # Affichage du texte chiffré
-print(f"Texte chiffré : {ciphertext}")
+print("Le texte chiffré est :", encrypted_text)
+
